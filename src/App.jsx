11ed4e9937a59770a71fda3391228a9b36fec611
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-10-16 10:38:15 +0200

    Creating a chessboard component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,24 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  // writing scrips here for now, will move out certain bits
+
+  const generateBoard = () => {
+    let array = [];
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        array.push(`${i}${j}`);
+      }
+    }
+    console.log(array);
+    return array;
+  };
+
   return (
     <div className="mainContainer">
       <div className="heading">Knight moves</div>
       <div className="howto">Click on chessboard bla bla bla</div>
-      <div className="gameboard"></div>
+      <div className="gameboard" onClick={generateBoard}></div>
       <div className="results"></div>
     </div>
   );
